b8f5eb36e8094a43a3c7cf0102379922cfaacdd8
omar olvera <omar.rodriguez@mexmax-internet.com>
2017-09-18 10:44:39 -0500

    add initial project data

---
+++ src/pages/my-teams/my-teams.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+    templateUrl: 'build/pages/my-teams/my-teams.page.html'
+})
+export class MyTeamspage{
+    constructor(){
+    }
+    
+}
