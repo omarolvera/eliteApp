53b01ec80bd08507b9585203b01fe4e0a3df4a97
omar olvera <omar.rodriguez@mexmax-internet.com>
2017-09-22 17:58:44 -0500

    display favorite teams

--- src/pages/my-teams/my-teams.page.ts
+++ src/pages/my-teams/my-teams.page.ts
@@ -8,6 +8,23 @@ import { TournamentsPage } from '../pages';
     templateUrl: 'my-teams.page.html'
 })
 export class MyTeamspage{
+
+    favorites=[
+        {
+        team:{"id": 6182,
+        "name": "HC Elite 7th",
+        "coach": "Steve Michelotti"},
+        tournamentId:"89e13aa2-ba6d-4f55-9cc2-61eba6172c63",
+        tournamentName: "March Madness Tournament"
+    },
+    {
+        team:{"id": 805,
+        "name": "HC Elite",
+        "coach": "Steve Michelotti"},
+        tournamentId: "98c6857e-b0d1-4295-b89e-2d95a45437f2",
+        tournamentName: "Holiday Hoops Challenge"
+    }
+]
     constructor(private nav: NavController){
         
     }
